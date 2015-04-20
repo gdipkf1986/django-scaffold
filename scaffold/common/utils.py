b50ba4f0c263ebef6dabce9b9a3e6a5226033339
jovi <huangf@garena.com>
2015-04-20 18:06:33 +0800

    utils for most used function

---
+++ scaffold/common/utils.py
@@ -0,0 +1,8 @@
+__author__ = 'Jovi'
+
+import string
+import random
+
+
+def random_string(length=10, excludes='1lLiI0oO', source_string=(string.ascii_letters + string.digits)):
+    return ''.join([source_string[x] for x in random.sample(range(0, len(source_string)-1), length) if str(x) not in excludes])
