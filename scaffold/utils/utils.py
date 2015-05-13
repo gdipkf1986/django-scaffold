566acaef60b33dcd410a1a7a05534a9fff293f49
jovi <huangf@garena.com>
2015-05-13 17:02:07 +0800

    fix "if" condition

--- scaffold/utils/utils.py
+++ scaffold/utils/utils.py
@@ -5,4 +5,4 @@ import random
 
 
 def random_string(length=10, excludes='1lLiI0oO', source_string=(string.ascii_letters + string.digits)):
-    return ''.join([source_string[x] for x in random.sample(range(0, len(source_string)-1), length) if str(x) not in excludes])
+    return ''.join([source_string[x] for x in random.sample(range(0, len(source_string)-1), length) if str(source_string[x]) not in excludes])
