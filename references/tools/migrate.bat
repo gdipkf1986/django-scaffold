f69d2ebeb29451eea558d1ec00046d53d8afeb5a
jovi <huangf@garena.com>
2015-09-09 10:15:37 +0800

    update south migration

--- references/tools/migrate.bat
+++ references/tools/migrate.bat
@@ -1,8 +1,17 @@
 %~d0
 cd %~dp0
 cd ../../
+
 rem first time
-python manage.py syncdb
+rem python manage.py syncdb
 rem python manage.py convert_to_south gmc
-python manage.py schemamigration gmc --auto
-python manage.py migrate
+rem python manage.py schemamigration gmc --init
+
+
+rem after changed models
+rem following command will collect changes from last migration, so must initial if never init
+rem the changes will collected in <app>/migrations
+python manage.py schemamigration <appName> --auto
+
+rem following command will apply all changes in <app>/migrations
+python manage.py migrate <appName>
